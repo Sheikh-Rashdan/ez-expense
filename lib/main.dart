b73e3b5e82f73ae1bf000cafcf3cbcff145e215b
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-08-08 01:34:16 +0530

    Set Preferred Orientations to portraitUp and portraitDown

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:ez_expense/constants/colors.dart';
+import 'package:ez_expense/constants/text_styles.dart';
+import 'package:ez_expense/pages/home_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MainApp());
 }
 
