080949caff4420a251d276831049000735704d49
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-08-08 01:33:30 +0530

    Created static HomePage with AppbarContainer

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,66 @@
+import 'package:ez_expense/constants/text_styles.dart';
+import 'package:ez_expense/widgets/appbar_container.dart';
+import 'package:ez_expense/widgets/single_value_donut_chart.dart';
+import 'package:ez_expense/widgets/sliver_page_column.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SliverPageColumn(
+        children: [AppbarContainer(child: _HomePageAppbarWidget())],
+      ),
+    );
+  }
+}
+
+class _HomePageAppbarWidget extends StatelessWidget {
+  const _HomePageAppbarWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FittedBox(
+                child: Text("₹ 1,346/3,000", style: KTextStyles.medium36),
+              ),
+              Text.rich(
+                TextSpan(
+                  style: KTextStyles.light12,
+                  children: [
+                    TextSpan(text: "Spend up to "),
+                    TextSpan(text: "₹965", style: KTextStyles.bold12),
+                    TextSpan(
+                      text: " each week to stay within your expense target.",
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 80,
+          width: 80,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              SingleValueDonutChart(fraction: 0.45),
+              Align(
+                alignment: Alignment.center,
+                child: Text("45%", style: KTextStyles.regular16),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
