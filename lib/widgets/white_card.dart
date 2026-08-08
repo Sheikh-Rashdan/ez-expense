0126571a66d3d86ba5e03e64221239b00ff1400f
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-08-08 21:17:20 +0530

    Created WhiteCard widget

---
+++ lib/widgets/white_card.dart
@@ -0,0 +1,34 @@
+import 'package:ez_expense/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class WhiteCard extends StatelessWidget {
+  const WhiteCard({
+    super.key,
+    this.child,
+    this.borderRadius = 10,
+    this.padding = const EdgeInsets.all(10),
+  });
+
+  final Widget? child;
+  final double borderRadius;
+  final EdgeInsets padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: KColors.cardLight,
+        borderRadius: BorderRadius.circular(borderRadius),
+        boxShadow: const [
+          BoxShadow(
+            blurRadius: 8,
+            offset: Offset(0, 2),
+            color: KColors.shadowColor,
+          ),
+        ],
+      ),
+      padding: padding,
+      child: child,
+    );
+  }
+}
