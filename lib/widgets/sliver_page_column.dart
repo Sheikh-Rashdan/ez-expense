a9426f7396c4baa2dc69503c191096c7bf2ea27d
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-08-09 02:54:51 +0530

    Added leading widget to SliverPageColumn appbar to open navigation drawer

--- lib/widgets/sliver_page_column.dart
+++ lib/widgets/sliver_page_column.dart
@@ -1,3 +1,5 @@
+import 'package:ez_expense/constants/colors.dart';
+import 'package:ez_expense/constants/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class SliverPageColumn extends StatelessWidget {
@@ -12,6 +14,11 @@ class SliverPageColumn extends StatelessWidget {
   final List<Widget> children;
   final bool scrollable;
 
+  void _onNavigationMenuPressed(BuildContext context) {
+    if (!context.mounted) return;
+    Scaffold.of(context).openDrawer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
@@ -19,7 +26,23 @@ class SliverPageColumn extends StatelessWidget {
           ? const ClampingScrollPhysics()
           : const NeverScrollableScrollPhysics(),
       slivers: [
-        SliverAppBar(title: Text(title)),
+        SliverAppBar(
+          title: Text(title),
+          leading: IconButton(
+            onPressed: () => _onNavigationMenuPressed(context),
+            icon: Icon(
+              Icons.menu_rounded,
+              size: KTextStyles.regular28.fontSize,
+              shadows: [
+                Shadow(
+                  offset: Offset(0, 1.5),
+                  blurRadius: 2,
+                  color: KColors.shadow,
+                ),
+              ],
+            ),
+          ),
+        ),
         SliverList(delegate: SliverChildListDelegate(children)),
       ],
     );
